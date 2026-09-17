689dd1ae0d4c679530aff883f8c9d415c8d2c1a2
Sathish.Rajasekaran2023@gmail.com <sathish.rajasekaran2023@gmail.com>
2026-09-17 21:35:27 +0530

    Add Git practice file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,8 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        var viewModel = new GameViewModel();
-        return View(viewModel);
+        var viewMode = new GameViewModel();
+        return View(viewMode);
     }
 
     public IActionResult Privacy()
